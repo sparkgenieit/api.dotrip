70b0e79af7ed103f6d25bab4cc62bd5cdd9cf400
vintron-kiran <vintron@richlabz.com>
2025-05-11 16:37:23 +0530

    port changed

--- src/main.ts
+++ src/main.ts
@@ -16,6 +16,6 @@ async function bootstrap() {
   });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT , "0.0.0.0");
 }
 bootstrap();
