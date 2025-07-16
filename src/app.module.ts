b4a701a3ab5497fe378ca3a8aa21a6aac6f0d589
avinash <avinashmallepula2600@gmail.com>
2025-07-16 15:06:24 +0530

    Feedback Invoice

--- src/app.module.ts
+++ src/app.module.ts
@@ -19,6 +19,7 @@ import { AdminModule } from './admin/admin.module';
 import { DriverModule } from './drivers/driver.module'; // ✅ Import Driver Module
 import { TripsModule } from './admin/trips/trips.module'; // adjust if in admin/trips
 import { FeedbackModule } from './feedback/feedback.module'; // ✅ NEW: Import Feedback Module
+import { InvoiceModule } from './invoice/invoice.module'; // ✅ NEW: Import Feedback Module
 
 
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
@@ -41,6 +42,7 @@ import { JwtAuthGuard } from './auth/jwt-auth.guard';
     TripsModule,
     DriverModule, // ✅ Register Driver Module here
     FeedbackModule, // ✅ NEW: Register Feedback Module
+    InvoiceModule,
     
   ],
   providers: [
